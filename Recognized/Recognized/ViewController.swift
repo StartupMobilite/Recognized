a760af44cbf84c97ebb72e88c367846a26f39e43
Karine Ouldbraham <k.ouldbraham@weezevent.com>
2015-12-02 17:20:53 +0100

    add image tab item

---
+++ Recognized/Recognized/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recognized
+//
+//  Created by Karine Ould on 02/12/2015.
+//  Copyright © 2015 Karine Ould. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

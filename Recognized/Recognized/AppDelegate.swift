3333a4819d61bfbbb9f3a550757f1b24cf1107d4
Karine Ouldbraham <k.ouldbraham@weezevent.com>
2015-12-18 01:35:16 +0100

    compte rendu 17/12/2015 + new specs du projet

--- Recognized/Recognized/AppDelegate.swift
+++ Recognized/Recognized/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print("Hello Word")
+        
         return true
     }
 
@@ -39,8 +41,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
 
+    }
 
 }
 

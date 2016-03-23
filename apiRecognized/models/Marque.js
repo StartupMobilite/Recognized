784d8bd78353163aaa722d3fd90926f363e93493
Sidbey <Ben.nikiema@ynov.com>
2016-03-23 22:32:34 +0100

    Model

---
+++ apiRecognized/models/Marque.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ben on 23/03/2016.
+ */
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var Marque;
+    Marque = sequelize.define('Marque', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        libelle: DataTypes.STRING,
+        logo: DataTypes.STRING,
+        personne: DataTypes.INTEGER
+    });
+    return Marque;
+};
